78157af31bd2c2c48ffd8479868ef47708cd61a9
unknown <ihou@ucsd.ed>
2021-02-22 13:46:51 -0800

    1

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
 app.get('/recipe', recipe.view);
+app.get('/index', index.view);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
