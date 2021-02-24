9e788cb8c90dfdb5c980c4e85099d781a5a32b39
unknown <ihou@ucsd.ed>
2021-02-24 11:40:57 -0800

    1

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,9 +2,10 @@
  * GET home page.
  */
   
-//var displaynames = require('../home.json');
+var displaynames = require('../home.json');
 
 exports.view = function(req, res){
+  console.log(displaynames);
   res.render('homepage', displaynames);
 
 }
